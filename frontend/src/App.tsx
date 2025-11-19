afb23ba1674418f33a2193a3d1854e1ddbd69487
harwarl <harwarl87@gmail.com>
2025-11-19 13:57:26 +0100

    wallet connect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,60 +1,3 @@
-// import { Toaster } from "./components/ui/toaster";
-// import { Toaster as Sonner } from "./components/ui/sonner";
-// import { TooltipProvider } from "./components/ui/tooltip";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Index from "./pages/Index";
-// import NotFound from "./pages/NotFound";
-
-// // SOLANA Wallet Adapter Setup
-// import {
-//   ConnectionProvider,
-//   WalletProvider,
-// } from "@solana/wallet-adapter-react";
-// import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-
-// import {
-//   PhantomWalletAdapter,
-//   SolflareWalletAdapter,
-//   // BackpackWalletAdapter
-// } from "@solana/wallet-adapter-wallets";
-
-// const queryClient = new QueryClient();
-
-// const App = () => {
-//   const endpoint = "https://api.mainnet-beta.solana.com";
-
-//   const wallets = [
-//     new PhantomWalletAdapter(),
-//     new SolflareWalletAdapter(),
-//     // new BackpackWalletAdapter(),
-//   ];
-
-//   return (
-//     <QueryClientProvider client={queryClient}>
-//       <TooltipProvider>
-//         <Toaster />
-//         <Sonner />
-
-//         <ConnectionProvider endpoint={endpoint}>
-//           <WalletProvider wallets={wallets} autoConnect>
-//             <WalletModalProvider>
-//               <BrowserRouter>
-//                 <Routes>
-//                   <Route path="/" element={<Index />} />
-//                   <Route path="*" element={<NotFound />} />
-//                 </Routes>
-//               </BrowserRouter>
-//             </WalletModalProvider>
-//           </WalletProvider>
-//         </ConnectionProvider>
-//       </TooltipProvider>
-//     </QueryClientProvider>
-//   );
-// };
-
-// export default App;
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "./components/ui/toaster";
@@ -71,10 +14,7 @@ import {
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
-import {
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { useMemo } from "react";
 import { clusterApiUrl } from "@solana/web3.js";
 // Default styles that can be overridden by your app
